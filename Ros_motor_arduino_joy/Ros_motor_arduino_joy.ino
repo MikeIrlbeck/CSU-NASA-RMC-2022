e3d0a795bf6f33a5a2026a38f6f260c297bc1c93
Dylan <91281812+DylanClem@users.noreply.github.com>
2022-04-12 12:49:48 -0600

    Update Ros_motor_arduino_joy.ino

--- Ros_motor_arduino_joy/Ros_motor_arduino_joy.ino
+++ Ros_motor_arduino_joy/Ros_motor_arduino_joy.ino
@@ -14,24 +14,46 @@ ros::NodeHandle nh;
 
 void callback(const sensor_msgs::Joy& joy)
 { 
-  int joy0 = joy.axes[0] * 125; //LS LR
-  int joy1 = joy.axes[1] * 125; //LS UD
-  int joy2 = -127.5 * (joy.axes[2] - 1); //LT
-  int joy3 = joy.axes[3] * 125; //RS LR
-  int joy4 = joy.axes[4] * 125; //RS UD
+  int p = 80;
+  int joy0 = joy.axes[0] * p; //LS LR
+  int joy1 = joy.axes[1] * p; //LS UD
+  int joy2 = -8 * (joy.axes[2] - 1); //LT
+  int joy3 = joy.axes[3] * p; //RS LR
+  int joy4 = joy.axes[4] * p; //RS UD
   int joy5 = -127.5 * (joy.axes[5] - 1); //RT
   int button1 = joy.buttons[1]; //B button
-
-  ST1.motor(1, joy1);// Wheels drive
-  ST0.motor(1, joy1);
-  ST1.motor(2, joy1);
-  ST0.motor(2, joy1);
+  int button0 = joy.buttons[0];
+  int button2 = joy.buttons[2];
+  int button3 = joy.buttons[3];
   
-  ST2.motor(2, -joy3);
-  ST3.motor(1, joy3);
-  //ST3.motor(2, joy4);
-  //ST2.motor(1, joy4);
-  ST4.motor(joy2); //BIG FAT DUMPY
+  if (button2==1){
+    ST0.motor(1, -joy1);
+    ST0.motor(2, joy1);
+    ST1.motor(1, joy1);
+    ST1.motor(2, -joy1);
+  }
+  else{
+    ST0.motor(1, -joy0);
+    ST0.motor(2, joy0);
+    ST1.motor(1, -joy0);
+    ST1.motor(2, joy0);
+  }
+  
+  
+  ST2.motor(1, -joy3);
+  ST3.motor(2, joy3);
+
+  if (button3==1){
+    ST3.motor(1, joy4);
+    ST2.motor(2, joy4);
+  }
+  else {
+    ST3.motor(1, joy4);
+    ST2.motor(2, -joy4);
+  }
+  
+  if (button0==1) ST4.motor(-joy2);
+  else ST4.motor(joy2); //BIG FAT DUMPY
   // If button B is pressed Reverse else forward
   
   if (button1==1) digitalWrite(3, HIGH);
@@ -56,6 +78,7 @@ void setup()
     ST2.turn(0);
     ST3.drive(0);
     ST3.turn(0);
+    ST4.motor(0);
 
     
     pinMode(2, OUTPUT); // PWM for belt speed
